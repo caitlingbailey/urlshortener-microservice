5ed4bfcce733b30917c025045db1fd0d32c7ddac
Caitlin <>
2021-05-25 19:10:46 +0000

    Create POST method

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
@@ -9,6 +10,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -21,4 +24,17 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+// Create URL Shortener
+
+app.get("/api/shorturl", (req, res) => {
+  res.json({output: req});
+})
+
+app.post("/api/shorturl", (req, res) => {
+  const url = req.query;
+  console.log(req);
+  console.log(req.body);
+  res.json({"output": req.body});
 });
