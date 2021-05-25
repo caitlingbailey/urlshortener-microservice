f776bf5f43defd401ea66469dfe1b83a0c3cdd20
Caitlin <>
2021-05-25 22:55:34 +0000

    Updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -16,6 +21,28 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+// Create new schema instance
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema({
+  original_url: { type: String, required: true },
+  short_url: {type: Number}
+});
+
+// Create instance of new schema
+const URL = mongoose.model("URL", urlSchema);
+
+// Method to create new URL
+const createShortURL = (original_url, done) => {
+  let current_url = new URL({original_url: original_url});
+  
+  current_url.save(function(err, data) {
+    if (err) return console.error(err);
+    console.log(data);
+    done(null, data)
+  });
+};
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
@@ -33,8 +60,15 @@ app.get("/api/shorturl", (req, res) => {
 })
 
 app.post("/api/shorturl", (req, res) => {
-  const url = req.query;
-  console.log(req);
-  console.log(req.body);
-  res.json({"output": req.body});
+  // console.log(req);
+  console.log(req.body.url);
+  
+  dns.lookup(req.body.url, (err, address, family) => {
+    if (err) return 1;
+  });
+
+  createShortURL(req.body.url);
+
+  res.json({ original_url : req.body.url,
+  short_url : 1});
 });
